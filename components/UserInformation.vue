c830401b20192d43f28ef35ddb5a97ee04d771ff
Ahmet Korkmaz <muratahmetkorkmaz@hotmail.com>
2022-01-01 16:16:04 +0300

    Add github request for contributions data

--- components/UserInformation.vue
+++ components/UserInformation.vue
@@ -1,21 +1,37 @@
 <template>
   <div class="pt-24 flex flex-col justify-center mb-16">
     <div class="inputs flex flex-row">
-      <input type="text" id="github-username"
+      <input type="text" id="github-username" v-model="githubUsername"
+             v-on:keyup.enter="sendUsernameData"
              class="mb-4 mr-4 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
              placeholder="GitHub Username" required>
 
-      <input type="text" id="gitlab-username"
+      <input type="text" id="gitlab-username" v-model="gitlabUsername"
+             v-on:keyup.enter="sendUsernameData"
              class="mb-4 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
              placeholder="Gitlab Username" required>
     </div>
 
-    <button type="button" class="bg-indigo-600 text-white text-sm leading-6 font-medium py-2 px-3 rounded-lg">Merge your graph</button>
+    <button type="button"
+            @click="sendUsernameData"
+            class='bg-indigo-600 text-white text-sm leading-6 font-medium py-2 px-3 rounded-lg'>Merge your graph
+    </button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'UserInformation'
+  name: 'UserInformation',
+  data() {
+    return {
+      githubUsername: '',
+      gitlabUsername: '',
+    };
+  },
+  methods: {
+    sendUsernameData() {
+      this.$emit('calculateData', { githubUsername: this.githubUsername, gitlabUsername: this.gitlabUsername });
+    }
+  }
 }
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="antialiased text-gray-400 bg-gray-900 h-screen flex flex-col justify-center px-56">
-    <UserInformation />
+    <UserInformation @calculateData="calculateGraph" />
     <Contributions />
   </div>
 </template>
@@ -8,5 +8,48 @@
 <script>
 export default {
   name: 'IndexPage',
+  methods: {
+    calculateGraph({ githubUsername, gitlabUsername }) {
+      this.getGithubContributions(githubUsername)
+      // this.getGitlabContributions(gitlabUsername)
+    },
+    getGithubContributions(username) {
+      this.$axios.post('https://api.github.com/graphql', {
+        query: `query {
+                  user(login: "${username}"){
+                    contributionsCollection {
+                      contributionCalendar {
+                        totalContributions
+                        weeks {
+                        contributionDays {
+                            contributionCount
+                            date
+                          }
+                        }
+                      }
+                    }
+                  }
+                }`
+      }, {
+        headers: {
+          'Authorization': `Bearer ${process.env.NUXT_ENV_GITHUB_PERSONAL_KEY}`,
+        }
+      }).then(res => {
+        console.log(res)
+      })
+    },
+    getGitlabContributions(username) {
+      this.$axios.get(`https://gitlab.com/users/${username}/calendar.json`, {
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        }
+      }).then(res => {
+        console.log(res)
+      }).catch(err => {
+        console.log(err)
+      })
+    }
+  }
 }
 </script>
