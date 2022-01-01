92b84efd61712c9d80cda1f265db26b2df6201df
Ahmet Korkmaz <muratahmetkorkmaz@hotmail.com>
2022-01-01 15:36:13 +0300

    Add contributions graph with sample data

--- components/UserInformation.vue
+++ components/UserInformation.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="pt-24 flex flex-col justify-center">
+  <div class="pt-24 flex flex-col justify-center mb-16">
     <div class="inputs flex flex-row">
       <input type="text" id="github-username"
              class="mb-4 mr-4 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
-  <div class="antialiased text-gray-400 bg-gray-900 h-screen flex justify-center">
+  <div class="antialiased text-gray-400 bg-gray-900 h-screen flex flex-col justify-center px-56">
     <UserInformation />
+    <Contributions />
   </div>
 </template>
 
